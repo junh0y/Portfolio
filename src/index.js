fe492b3b96ae455a9deb64dfc496f5b7708679e9
junh0y <y.junh0y@gmail.com>
2024-07-21 20:59:03 +0100

    Basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import systemText from './systemText.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename='/Portfolio'>
     {/* <App /> */}
     <Routes>
       <Route path='/'>
